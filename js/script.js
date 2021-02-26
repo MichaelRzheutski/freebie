15a9efc83ed2e17f53697f6c3f700f0a6a43e25a
MichalRzavucki <michael.rzheutski@gmail.com>
2021-02-27 01:48:01 +0300

    Whole project has been created

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
 $(function() {
 
-  /* Menu nav toggle */
+  let header = $("#nav-wrap"),
+      introH = $("#nav-wrap").innerHeight(),
+      scrollOffset = $(window).scrollTop();
+
+  // Fixed Header
+  checkScroll(scrollOffset);
+
+  $(window).on("scroll", function() {
+
+    scrollOffset = $(this).scrollTop();
+    checkScroll(scrollOffset);
+
+  });
+
+  function checkScroll(scrollOffset) {
+    if ( scrollOffset >= introH ) {
+      header.addClass("fixed");
+    } else {
+      header.removeClass("fixed");
+    }
+  };
+
+
+  // Menu nav toggle
   $("#nav-toggle").on("click", function(event){
     event.preventDefault();
 
@@ -8,14 +31,79 @@ $(function() {
     $("#nav").toggleClass("active");
   });
 
-  /* Collapse */
+
+  // Smooth Scroll to elements
+  $("[data-scroll]").on("click", function(event){
+    event.preventDefault();
+
+    let $this = $(this),
+        blockId = $this.data("scroll"),
+        blockOffset = $(blockId).offset().top - 65;
+
+    $("#nav a").removeClass("active");
+    $this.addClass("active");
+
+    $("html, body").animate({
+      scrollTop: blockOffset
+    }, 500);
+  });
+
+
+  // Slider
+  $("[data-slider]").slick({
+      infinite: true,
+      fade: false,
+      slidesToShow: 1,
+      slidesToScroll: 1
+  });
+
+
+  // Collapse
   $("[data-collapse]").on("click", function(event) {
       event.preventDefault();
 
-      var $this = $(this),
+      let $this = $(this),
           blockId = $this.data('collapse');
 
       $this.toggleClass("active");
   });
 
+
+  // ToTop Button
+  let btn = $('#to-top');
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 1) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
+
 });
+
+
+// Price Switcher
+function priceSwitch() {
+  let checkbox = document.getElementById('dn');
+  let text1 = document.getElementsByClassName('text1');
+  let text2 = document.getElementsByClassName('text2');
+
+  for (let i = 0; i < text1.length; i++) {
+
+    if (!checkbox.checked) {
+      text1[i].style.display = 'grid';
+      text2[i].style.display = 'none';
+    } else {
+      text1[i].style.display = 'none';
+      text2[i].style.display = 'grid';
+    }
+
+  }
+
+}
